8e71afc8589cf05bd82f470d96bcdf9e78f79877
pushpendrakraut <pushpendrakraut@gmail.com>
2025-12-10 15:24:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false, // For simple IPC in MVP
+        },
+    });
+
+    // In development, load from Vite dev server
+    // In production, load from built files (not implemented for MVP yet)
+    win.loadURL('http://localhost:5173');
+
+    // Open DevTools
+    // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
